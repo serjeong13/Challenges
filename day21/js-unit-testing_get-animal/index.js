e7425dae53e8ad7e58a63e57de6c4c9c22efed1f
SERGEY <137767033+serjeong13@users.noreply.github.com>
2023-07-24 16:07:48 +0200

    crazy

---
+++ day21/js-unit-testing_get-animal/index.js
@@ -0,0 +1,9 @@
+export function getAnimal(animalPluralName) {
+  if (animalPluralName === "cats") {
+    return "I totally love cats!";
+  } else if (animalPluralName) {
+    return `I like ${animalPluralName}!`;
+  } else {
+    return "I do not like animals at all!";
+  }
+}
