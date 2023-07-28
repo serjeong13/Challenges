cad6fb4a2d03bc4ddca9cec8d00716779f23d83d
SERGEY <137767033+serjeong13@users.noreply.github.com>
2023-07-28 10:42:40 +0200

    ne

---
+++ day21/js-unit-testing_people/index.js
@@ -0,0 +1,21 @@
+export function getFirstNames(people) {
+  return people.map((person) => person.firstName);
+}
+
+export function getFullNames(people) {
+  return people.map((person) => person.firstName + " " + person.lastName);
+}
+
+export function getNameAndAge(people) {}
+
+export function getPeopleByAge(people, age) {}
+
+export function getPeopleNamesOlderThan(people, age) {}
+
+export function getPeopleByLastName(people, lastName) {}
+
+export function findPersonById(people, id) {}
+
+export function isAnyoneOlderThan(people, age) {}
+
+export function getFullNamesSortedByAge(people) {}
---
+++ day21/js-unit-testing_people/index.test.js
@@ -0,0 +1,81 @@
+import {
+  getFirstNames,
+  getFullNames,
+  getNameAndAge,
+  getPeopleByAge,
+  getPeopleNamesOlderThan,
+  getPeopleByLastName,
+  findPersonById,
+  isAnyoneOlderThan,
+  getFullNamesSortedByAge,
+} from "./index.js";
+
+const people = [
+  {
+    id: 1,
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 21,
+  },
+  {
+    id: 2,
+    firstName: "John",
+    lastName: "Doe",
+    age: 18,
+  },
+  {
+    id: 3,
+    firstName: "Max",
+    lastName: "Mustermann",
+    age: 32,
+  },
+  {
+    id: 4,
+    firstName: "Erika",
+    lastName: "Musterfrau",
+    age: 38,
+  },
+  {
+    id: 5,
+    firstName: "Luke",
+    lastName: "Skywalker",
+    age: 23,
+  },
+];
+
+test("getFirstNames() returns an array with the first names", () => {
+  const result = getFirstNames(people);
+  expect(result).toEqual(["Jane", "John", "Max", "Erika", "Luke"]);
+});
+
+test('getFullNames() returns an array with full names as strings, e.g. "Jane Doe"', () => {
+  const result = getFullNames(people);
+  expect(result).toEqual([
+    "Jane Doe",
+    "John Doe",
+    "Max Mustermann",
+    "Erika Musterfrau",
+    "Luke Skywalker",
+  ]);
+});
+
+test('getNameAndAge() returns an array of formatted strings like this: "Skywalker (23)"', () => {
+  const result = getNameandAge(people);
+  expect(result).toEqual([Doe ()])
+});
+
+test("getPeopleByAge() returns an array of people of a certain age", () => {});
+
+test('getPeopleNamesOlderThan() returns an array of people full names older than a certain age, e.g. "Luke Skywalker"', () => {});
+
+test("getPeopleByLastName() returns an array of people of a certain lastName", () => {});
+
+// Hint: use Array.prototype.find for the implementation
+test("findPersonById() returns the full object of a person", () => {});
+
+// Hint: implementation should use Array.prototype.some
+test("isAnyoneOlderThan() returns true if any person is older than the given age", () => {});
+
+test("isAnyoneOlderThan() returns false if no person is older than the given age", () => {});
+
+test("getFullNamesSortedByAge() returns an array of last names sorted by age", () => {});
