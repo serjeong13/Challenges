272240e004cb6e409fa71c7e6e0ead536bd11041
SERGEY <137767033+serjeong13@users.noreply.github.com>
2023-07-28 17:56:04 +0200

    completed

--- day24/react-props_button/src/App.js
+++ day24/react-props_button/src/App.js
@@ -2,5 +2,36 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  const handleClick = () => {
+    console.log("Rocco is a champion");
+    alert("You clicked me hard, BABY!");
+  };
+
+  return (
+    <div>
+      <Button
+        onClick={handleClick}
+        color="blue"
+        disabled={false}
+        text="Click me hard, BABY"
+      />
+    </div>
+  );
+}
+
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        width: "100px",
+        height: "100px",
+        backgroundColor: color,
+        color: "yellow",
+      }}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 }
