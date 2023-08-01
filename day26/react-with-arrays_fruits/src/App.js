c01f2aec1c87406d6f63210d9ab2fef7c9ae3639
SERGEY <137767033+serjeong13@users.noreply.github.com>
2023-08-01 15:40:16 +0200

    crazy fruits, why?

--- day26/react-with-arrays_fruits/src/App.js
+++ day26/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,19 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1, name: "apple", colour: "yellow" },
+    { id: 2, name: "strawberry", colour: "pink" },
+    { id: 3, name: "watermelon", colour: "red" },
+    { id: 4, name: "orange", colour: "blue" },
+    { id: 5, name: "kiwi", colour: "green" },
+  ];
 
   return (
-    <div className="app">
-      <Card name="🍌 banana" />
-    </div>
+    <ul>
+      {fruits.map(({ id, name, colour }) => (
+        <Card key={id} name={name} colour={colour} />
+      ))}
+    </ul>
   );
 }
