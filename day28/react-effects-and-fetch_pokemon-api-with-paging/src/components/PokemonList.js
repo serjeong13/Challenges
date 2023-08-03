247e2ea2dd451519e6988107c8f40996f38ea6bd
SERGEY <137767033+serjeong13@users.noreply.github.com>
2023-08-03 16:16:07 +0200

    d

--- day28/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ day28/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -2,15 +2,19 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
+
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
 
   useEffect(() => {
     async function loadPokemon() {
       try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
+        const response = await fetch(initialUrl);
         const data = await response.json();
         setPokemon(data.results);
+        setNextUrl(data.next);
+        setPrevUrl(data.previous);
       } catch (error) {
         console.log(error);
       }
@@ -21,8 +25,12 @@ export default function PokemonList() {
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={() => prevUrl && loadPokemon(prevUrl)}>
+        Previous Page
+      </button>
+      <button type="button" onClick={() => nextUrl && loadPokemon(nextUrl)}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
