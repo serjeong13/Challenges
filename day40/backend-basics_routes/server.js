c38dda910a6c68756f3bd9f44a4ee4dda88ba711
SERGEY <137767033+serjeong13@users.noreply.github.com>
2023-08-21 15:36:59 +0200

    challenge2

--- day40/backend-basics_routes/server.js
+++ day40/backend-basics_routes/server.js
@@ -1,11 +1,11 @@
 import { createServer } from "node:http";
 
-const port = 8000;
-server.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`);
-});
-
-export const server = createServer((request, response) => {
-  response.statusCode = 200;
-  response.end("Hello World");
+export const server = createServer((req, res) => {
+  if (req.url === "/api/fish/1") {
+    res.statusCode = 200;
+    res.end("Shrimp");
+  } else {
+    res.statusCode = 404;
+    res.end("Not found");
+  }
 });
